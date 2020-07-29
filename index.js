0bf38bc5dbac20a962d7d25e6ed8dd9d691e89ab
ashleyaxl <ashansh2012@gmail.com>
2020-07-30 02:59:35 +0530

    app init

---
+++ index.js
@@ -0,0 +1,66 @@
+var skylink = new Skylink();
+
+skylink.on('peerJoined', function(peerId, peerInfo, isSelf) {
+  var user = 'You';
+  if(!isSelf) {
+    user = peerInfo.userData.name || peerId;
+  }
+  addMessage(user + ' joined the room', 'action');
+});
+
+skylink.on('peerUpdated', function(peerId, peerInfo, isSelf) {
+  if(isSelf) {
+    user = peerInfo.userData.name || peerId;
+    addMessage('You\'re now known as ' + user, 'action');
+  }
+});
+
+skylink.on('peerLeft', function(peerId, peerInfo, isSelf) {
+  var user = 'You';
+  if(!isSelf) {
+    user = peerInfo.userData.name || peerId;
+  }
+  addMessage(user + ' left the room', 'action');
+});
+
+skylink.on('incomingMessage', function(message, peerId, peerInfo, isSelf) {
+  var user = 'You',
+      className = 'you';
+  if(!isSelf) {
+    user = peerInfo.userData.name || peerId;
+    className = 'message';
+  }
+  addMessage(user + ': ' + message.content, className);
+});
+
+skylink.init('cd7819c8-8513-4c3f-8ed1-bc1a35bc13ba'); // Get your own key at developer.temasys.com.sg
+
+function setName() {
+  var input = document.getElementById('name');
+  skylink.setUserData({
+    name: input.value
+  });
+}
+
+function joinRoom() {
+  skylink.joinRoom();
+}
+
+function leaveRoom() {
+  skylink.leaveRoom();
+}
+
+function sendMessage() {
+  var input = document.getElementById('message');
+  skylink.sendP2PMessage(input.value);
+  input.value = '';
+  input.select();
+}
+
+function addMessage(message, className) {
+  var chatbox = document.getElementById('chatbox'),
+    div = document.createElement('div');
+  div.className = className;
+  div.textContent = message;
+  chatbox.appendChild(div);
+}
